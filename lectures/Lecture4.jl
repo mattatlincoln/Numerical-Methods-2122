4add877a7fdeb4c8579a1800813f465fbe789163
mattatlincoln <mwatkins@lincoln.ac.uk>
2021-11-03 14:31:41 +0000

    lecture 4

---
+++ lectures/Lecture4.jl
@@ -0,0 +1,13 @@
+### A Pluto.jl notebook ###
+# v0.17.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 01c34930-3c92-11ec-304f-6d6a4f6741b5
+md"""
+# Lecture 4
+"""
+
+# ╔═╡ Cell order:
+# ╠═01c34930-3c92-11ec-304f-6d6a4f6741b5
